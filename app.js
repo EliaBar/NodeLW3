58e4600259eeb3ea9cb0f3558aa7915560780fdf
EliaBar <elinabaranevych@gmail.com>
2025-03-22 22:17:58 +0200

    Second commit

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const authController = require('./controllers/authController');
 const usersController = require('./controllers/usersController');
+const adminRouter = require('./services/studentsService');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(adminRouter);
 
 app.get('/login', authController.loginPage);
 app.post('/login', authController.login);
