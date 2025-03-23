1e516d33aac3659176152c5d9fd9006c73a79934
EliaBar <elinabaranevych@gmail.com>
2025-03-23 12:28:53 +0200

    Forth commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,36 +1,20 @@
 const authService = require('../services/authService');
-
-exports.loginPage = (req, res) => {
-    res.render('login', { error: null });
-};
-
-exports.login = (req, res) => {
-const express = require('express');
-const router = express.Router();
-const authService = require('../services/authService');
-}
-
-router.get('/login', (req, res) => {
-    res.render('login');
-});
-
-router.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = authService.authenticate(username, password);
-
-    if (user) {
-        if (user.role === 'admin') {
-            res.redirect('/admin');
-        } else {
-            res.redirect('/student');
-        }
-    } else {
-        res.render('login', { error: 'Невірний логін або пароль' });
-    }
-});
-
-router.post('/admin', (req, res)=>{
-    res.render('admin', { error: null });
-});
-
-module.exports = router;
+ 
+ exports.loginPage = (req, res) => {
+     res.render('login', { error: null });
+ };
+ 
+ exports.login = (req, res) => {
+     const { username, password } = req.body;
+     const user = authService.authenticate(username, password);
+ 
+     if (user) {
+         if (user.role === 'admin') {
+             res.redirect('/admin');
+         } else {
+             res.redirect('/student');
+         }
+     } else {
+         res.render('login', { error: 'Невірний логін або пароль' });
+     }
+ };
