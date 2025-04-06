07082412bfb2ed2d5109fb0439eb53b87e690700
Lytovchenkoo <anhelina.lytovchenkoo@gmail.com>
2025-04-06 14:28:19 +0300

    added styles and functionality

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const authController = require('./controllers/authController');
-const usersController = require('./controllers/usersController');
-const adminRouter = require('./services/studentsService');
+const authController = require("./controllers/authController");
+const usersController = require("./controllers/usersController");
+const adminRouter = require("./services/studentsService");
 
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(adminRouter);
 
-app.get('/login', authController.loginPage);
-app.post('/login', authController.login);
-app.get('/admin', usersController.adminPage);
-app.get('/student', usersController.studentPage);
+app.get("/login", authController.loginPage);
+app.post("/login", authController.login);
+app.get("/admin", usersController.adminPage);
+app.get("/student", usersController.studentPage);
 
 app.listen(3000, () => {
-    console.log('Сервер працює на http://localhost:3000');
+  console.log("Сервер працює на http://localhost:3000");
 });
