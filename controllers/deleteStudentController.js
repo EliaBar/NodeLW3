1d32b00b1a79ac238b21870ee6cce8e8161d1446
EliaBar <elinabaranevych@gmail.com>
2025-04-08 17:13:45 +0300

    Modify: deleyeStudentButton

--- controllers/deleteStudentController.js
+++ controllers/deleteStudentController.js
@@ -1,13 +1,19 @@
 const { getStudentsSync, saveStudentsSync } = require('../repositories/studentRepository');
 
 exports.deleteStudent = (req, res) => {
-  const { studentId } = req.body;
+  const { studentName, group } = req.body;
   const students = getStudentsSync();
 
-  for (const group in students) {
-    students[group] = students[group].filter((_, index) => index.toString() !== studentId);
+  console.log("studentName:", studentName);
+  console.log("group:", group);
+
+  if (!students[group]) {
+    return res.status(404).send('Group not found');
   }
 
+  // Видаляємо тільки в зазначеній групі
+  students[group] = students[group].filter(s => s.name !== studentName);
+
   saveStudentsSync(students);
-  res.redirect('/admin');
+  res.redirect(`/admin`);
 };
